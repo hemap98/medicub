fe9a258bf0e961e99127223fcd0f8eb3cccc5e15
hemap98 <hemapriyanka@gmail.com>
2018-12-27 22:53:14 +0530

    added cart and footer page,price and quantity calculation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    CartComponent,
+    ItemListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,14 @@
-import { constants } from 'os';
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 export const AppRoutes: Routes = [
     {path: '',redirectTo: 'login', pathMatch: 'full'},
-    {path: 'home', component:AppComponent},
+    {path: 'home', component:HomeComponent},
     {path: 'login', component:LoginComponent},
-    {path: 'signup', component:SignupComponent}
+    {path: 'signup', component:SignupComponent},
+    {path: 'cart', component:CartComponent},
 ]
